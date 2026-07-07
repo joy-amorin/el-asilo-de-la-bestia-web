093767f963bc016d4d110d2781f460c49dd9b31b
joy-amorin <amorinkaren2021@gmail.com>
2026-07-07 17:02:03 -0300

    style: remove the header column and leave the centered title

--- components/gallery/GallerySection.tsx
+++ components/gallery/GallerySection.tsx
@@ -8,32 +8,12 @@ export default function GallerySection() {
 
         {/* Header */}
 
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 items-center mb-20">
-
-          <div className="lg:col-span-6">
-
-            <h2 className="relative font-heading text-parchment text-5xl md:text-7xl lg:text-8xl uppercase leading-none">
-              En vivo
-              <span className="absolute left-0 -bottom-3 w-24 h-[2px] bg-blood"></span>
-            </h2>
-
-          </div>
-
-          <div className="lg:col-span-6">
-
-            <div className="border-l border-white/10 pl-8">
-
-              <p className="font-body text-steel-light text-lg leading-9">
-                Cada show es una batalla. Esta es la galería de nuestras
-                presentaciones en vivo. Caos, entrega y verdad.
-              </p>
-
-            </div>
-
-          </div>
-
+        <div className="mb-20 text-center">
+          <h2 className="relative font-heading text-parchment text-5xl md:text-7xl lg:text-8xl uppercase leading-none">
+            En vivo
+            <span className="absolute left-1/2 -translate-x-1/2 -bottom-3 w-24 h-[2px] bg-blood"></span>
+          </h2>
         </div>
-
         {/* Grid */}
 
         <GalleryGrid images={galleryImages} />
