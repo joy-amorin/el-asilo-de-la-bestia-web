6fa5d28e4136e6b44b4ebdfc8730b3a70274cb42
joy-amorin <amorinkaren2021@gmail.com>
2026-07-14 13:14:02 -0300

    refactor: sort discography by newest releases first

--- components/multimedia/data.js
+++ components/multimedia/data.js
@@ -1,33 +1,14 @@
 export const discography = [
   {
-    id: 1,
-    year: "2020",
-    type: "EP",
-    title: "EPidemia",
-    cover: "/multimedia/EPidemia.png",
-    description:
-      "Primer lanzamiento de la banda. Este EP reúne cinco canciones que marcaron el comienzo de su recorrido.",
-    spotify: "https://open.spotify.com/intl-es/track/6JNDzxH4reHMVH8RssXLpf?si=6cfc1f1a12754caa",
-  },
-  {
-    id: 2,
-    year: "2020",
+    id: 5,
+    year: "2025",
     type: "Single",
-    title: "Alfonsina y el mar",
-    cover: "/multimedia/Alfonsina.png",
+    title: "Marinero de luces",
+    cover: "/multimedia/Marinero-de-luces.png",
     description:
-      "Versión de un clásico popularizado por Mercedes Sosa, e interpretado con el sello de la banda.",
-    spotify: "https://open.spotify.com/intl-es/track/5XVPUbZ15TKyJWXF8ohQk3?si=ca6540c2a9584723",
-  },
-  {
-    id: 3,
-    year: "2023",
-    type: "Álbum",
-    title: "Despertar",
-    cover: "/multimedia/Despertar.png",
-    description:
-      "Primer álbum de estudio de la banda. Reúne once canciones originales",
-    spotify: "https://open.spotify.com/intl-es/album/3smBDwTFUY3LDbQO1aLDT0?si=zr06kceCRaKTJPxS9hoqpg",
+      "Una reinterpretación de un clásico de José Luis Perales, donde la sensibilidad de la obra original se fusiona con la fuerza y personalidad de la banda.",
+    spotify:
+      "https://open.spotify.com/intl-es/track/74JWqVaHxALN8H6tuUHu5d?si=6c69c3e5b5954658",
   },
   {
     id: 4,
@@ -37,17 +18,41 @@ export const discography = [
     cover: "/multimedia/Enemigos.png",
     description:
       "Sencillo lanzado en colaboración con Alejandro Spuntone.",
-    spotify: "https://open.spotify.com/intl-es/track/1gypBtZDCifC4YOmsGZuBw?si=edde552f0a9846de",
+    spotify:
+      "https://open.spotify.com/intl-es/track/1gypBtZDCifC4YOmsGZuBw?si=edde552f0a9846de",
   },
   {
-    id: 5,
-    year: "2025",
-    type: "Single",
-    title: "Marinero de luces",
-    cover: "/multimedia/Marinero-de-luces.png",
+    id: 3,
+    year: "2023",
+    type: "Álbum",
+    title: "Despertar",
+    cover: "/multimedia/Despertar.png",
     description:
-      "Una reinterpretación de un clásico de José Luis Perales, donde la sensibilidad de la obra original se fusiona con la fuerza y personalidad de la banda.",
-    spotify: "https://open.spotify.com/intl-es/track/74JWqVaHxALN8H6tuUHu5d?si=6c69c3e5b5954658",
+      "Primer álbum de estudio de la banda. Reúne once canciones originales.",
+    spotify:
+      "https://open.spotify.com/intl-es/album/3smBDwTFUY3LDbQO1aLDT0?si=zr06kceCRaKTJPxS9hoqpg",
+  },
+  {
+    id: 2,
+    year: "2023",
+    type: "Single",
+    title: "Alfonsina y el mar",
+    cover: "/multimedia/Alfonsina.png",
+    description:
+      "Versión de un clásico popularizado por Mercedes Sosa, e interpretado con el sello de la banda.",
+    spotify:
+      "https://open.spotify.com/intl-es/track/5XVPUbZ15TKyJWXF8ohQk3?si=ca6540c2a9584723",
+  },
+  {
+    id: 1,
+    year: "2020",
+    type: "EP",
+    title: "EPidemia",
+    cover: "/multimedia/EPidemia.png",
+    description:
+      "Primer lanzamiento de la banda. Este EP reúne cinco canciones que marcaron el comienzo de su recorrido.",
+    spotify:
+      "https://open.spotify.com/intl-es/track/6JNDzxH4reHMVH8RssXLpf?si=6cfc1f1a12754caa",
   },
 ];
 
